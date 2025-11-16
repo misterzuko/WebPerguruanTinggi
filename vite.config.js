24d1b9296e19dd97b72a35d583f3ce021be63c80
Daniro Prasetyo <mbahking02@gmail.com>
2025-11-16 20:56:41 +0700

    Development App Testing
    Warm up use Vite lol, with taillwind gangs

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { globSync } from 'glob';
+import tailwindcss from '@tailwindcss/vite';
+
+// Mengambil semua file HTML dari folder src/page
+const pageFiles = globSync('src/page/*.html');
+
+// Membuat objek input untuk Rollup
+const input = pageFiles.reduce((acc, file) => {
+  // Mendapatkan nama file tanpa ekstensi (e.g., 'about_us')
+  const name = file.split('/').pop().replace('.html', '');
+  acc[name] = resolve(__dirname, file);
+  return acc;
+}, {});
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Jangan lupa daftarkan juga index.html utama di root
+        main: resolve(__dirname, 'index.html'),
+        // Sebarkan (spread) semua file HTML dari folder page
+        ...input,
+      },
+    },
+  },
+  plugins: [tailwindcss()]
+});
